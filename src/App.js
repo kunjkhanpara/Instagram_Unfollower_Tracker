8dab8d7bea11273188bad56779985933b4e88ce1
kunjkhanpara <kunjpatel4647@gmail.com>
2024-12-25 16:08:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Correct imports for v6
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; // Import Link for navigation
 import './App.css';
 import FileUpload from './components/FileUpload';
 import Instructions from './components/Instructions'; // Assuming you've created this component
@@ -13,7 +13,13 @@ function App() {
         <div className="app-header">
           <h1>Followers & Following Comparison</h1>
           <p>Upload a ZIP file containing your followers and following data to find out who doesn't follow you back!</p>
+          
+          {/* Button to Start */}
+          <Link to="/" className="start-button">
+            <button>Start</button>
+          </Link>
         </div>
+
         <Routes>
           <Route path="/" element={<FileUpload />} />
           <Route path="/instructions" element={<Instructions />} />
